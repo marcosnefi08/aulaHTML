a88c7feff6f368e92bdd0779f0b4f6a76be0d2df
marcosnefi08 <nefisilva32@gmail.com>
2024-10-24 15:30:43 -0300

    24/10

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import (Flask, request) # Importa o flask
+from flask import (Flask, render_template, request) 
 
-app = Flask("__name__") # cria uma instância
+app = Flask(__name__)
 
-@app.route("/", methods=('GET',)) # Assina uma rota
-def index(): # função responsável pela página
-    return "<h1>Página inicial</h1>" "<P>lalala<P>" # HTML retornado
+@app.route("/", methods=('GET',))
+def index():
+    nome = request.args.get('nome')
+    return f"""<h1>Página inicial</h1> <p>Eu sou o Marcos</p>
+        <p>Olá {nome}, que nome bonito!
+    """
+@app.route("/galeria", methods=('GET',))
+def galeria():
+    return "<h1>Galeria</h1>"
+
+@app.route("/contato", methods=('GET',))
+def contato():
+    return "<h1>Contato (17) 99111-2323</h1>"
+
+@app.route("/sobre", methods=('GET',))
+def sobre():
+    return "<h1>Sobre...</h1>"
 
 @app.route("/area")
 def area():
@@ -13,17 +27,26 @@ def area():
   return f""" 
 <h1> A área informada> L={largura}* A={altura} Area={largura*altura}</h1>"""
 
-@app.route("/par_ou_impar", methods=('GET',))
-def par_ou_impar():
+@app.route("/parimpar", methods=('GET',))
+def parimpar():
   numero = float(request.args.get('n'))
   if numero % 2 == 0:
     return f"O número {numero} é par."
   else:
     return f"O número {numero} é ímpar."
   
-@app.route("/sobrenome", methods=('GET',))
-def nomesobrenome():
+@app.route("/nomesob", methods=('GET',))
+def nomesob():
   nome = request.args.get('nome')
   sobrenome = request.args.get('sobrenome')
-  return f"""<h1> sobrenome </h1>
+  return f"""<h1> Sobrenome </h1>
   <p>{sobrenome},{nome}</p>"""
+
+@app.route("/tabuada")
+@app.route("/tabuada/<numero>", methods=("GET", ))
+def tabuada(numero= None):
+
+  if 'numero' in request.args:
+      numero= request.args.get('numero')
+  
+  return render_template('tabuada.html', numero=numero)
